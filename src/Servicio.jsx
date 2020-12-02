4ff476dea586ff12bbe464bd57eb5e00b11d7c31
nachomartinez30 <nachomartinez3010@gmail.com>
2020-12-02 11:27:11 -0600

    responsive design

--- src/Servicio.jsx
+++ src/Servicio.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./assets/styles.css";
-const Servicio = ({ tittle, description }) => {
+const Servicio = ({ tittle, description, icon }) => {
 
     const [animacion, setAnimacion] = useState(false)
     const [showDescription, setShowDescription] = useState(false)
@@ -17,17 +17,15 @@ const Servicio = ({ tittle, description }) => {
     }
 
     return (
-        <div className={`xl:w-1/3 md:w-1/2 p-4 ${animacion ? 'slide-fwd-center' : null}`}
+        <div className={`xl:w-1/3 md:w-1/2 w-full p-4 ${animacion ? 'slide-fwd-center' : null}`}
             onMouseOver={handleMouseOver}
             onMouseLeave={handleMouseLeave}
         >
             <div className="services_shadows p-6 rounded-lg">
-                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-pink-100 text-pink-500 mb-4">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-6 h-6" viewBox="0 0 24 24">
-                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
+                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full text-amarillo-titulo mb-4">
+                    <img alt='' src={icon} />
                 </div>
-                <h2 className="text-2xl text-amarillo-titulo font-medium title-font mb-2">{tittle}</h2>
+                <h2 className="text-lg text-amarillo-titulo font-medium title-font mb-2">{tittle}</h2>
                 {
                     (showDescription) &&
                     <p className="leading-relaxed text-base fade-in">
--- src/Servicios.jsx
+++ src/Servicios.jsx
@@ -1,5 +1,15 @@
 import './assets/styles.css'
 import Servicio from "./Servicio";
+import citas from './assets/img/007-dieta.svg'
+import asesoria from './assets/img/001-comida.svg'
+import coach from './assets/img/002-manzana.svg'
+import psiconutricion from './assets/img/003-autismo.svg'
+import habitos from './assets/img/004-saltar-la-cuerda.svg'
+import antropometria from './assets/img/005-cinta-metrica.svg'
+import floral from './assets/img/006-lotus-flower.svg'
+import emocional from './assets/img/008-medios-de-comunicacion-social.svg'
+import plan from './assets/img/009-vitaminas.svg'
+import venta from './assets/img/001-comida.svg'
 
 const Servicios = () => {
     return (
@@ -9,45 +19,59 @@ const Servicios = () => {
                     <h1 className='text-4xl text-rosa  text-center'>Servicios</h1>
                     {/*Imagen SVG */}
                     <div className='flex flex-wrap '>
+  
                         <Servicio
+                            icon={citas}
                             tittle={'Citas Online y presenciales'}
-                            description='Lorem'
+                            description='Empieza a mejorar la relación que tienes con tu cuerpo y con la comida . En cualquier lugar de México y America Latina . Servicio de citas On line Presencial Guadalajara, Jalisco México'
                         />
                         <Servicio
+                            icon={asesoria}
                             tittle={'Asesoría nutricional'}
-                            description='Lorem'
+                            description='La mejor manera de cambiar tus hábitos es con una asesoría y continúa educación en nutrición.'
                         />
                         <Servicio
+                            icon={coach}
                             tittle={'Coach nutricional'}
-                            description='Lorem'
+                            description='Si dejas que nuestros coaches nutricionales te acompañen, conseguirás:
+                                Conocer qué alimentos debes tomar de forma fácil y natural. Comprender el vínculo entre tus kilos y las emociones Aprender a preparar tus propios menús fácilmente y disfrutarás haciéndolo. Mejorar la confianza en ti mismo/a y en tu cuerpo Aumentar tu vitalidad y te sentirás más en forma a medida que te acercas a tu peso ideal.'
                         />
                         <Servicio
+                            icon={psiconutricion}
                             tittle={'Psiconutrición'}
-                            description='Lorem'
+                            description='Relación de la mente, creencias, emociones y la nutrición.
+                                        Aborda temas como miedo a subir de peso, autoestima, espíritu para llevar a ser una mejor versión de sí mismo, trastornos alimenticios, ansiedad por comer y patrones alimenticios.'
                         />
                         <Servicio
+                            icon={habitos}
                             tittle={'Cambio de hábitos'}
-                            description='Lorem'
+                            description='Nuestra alimentación se basa principalmente en conductas que llevamos años realizando, y nos resulta muy complicado cambiar estos comportamientos debido a que se han convertido en nuestros hábitos.
+                            ¡Pero conseguir cambiar tus hábitos alimenticios para alcanzar unos más saludables es posible! En Armonía Nutritiva te ayudamos a lograrlo.'
                         />
                         <Servicio
+                            icon={antropometria}
                             tittle={'Antropometría certificada'}
-                            description='Lorem'
+                            description='Mediciones del cuerpo para conocer el porcentaje exacto de grasa de tu cuerpo y el porcentaje de masa muscular.'
                         />
                         <Servicio
+                            icon={floral}
                             tittle={'Terapia floral'}
-                            description='Lorem'
+                            description='Las Terapias Florales te ayudan a armonizar tus emociones para equilibrarte física y emocionalmente.'
                         />
                         <Servicio
+                            icon={emocional}
                             tittle={'Nutricion emocional'}
-                            description='Lorem'
+                            description='Nuestras emociones juegan un papel importante y se reflejan en la manera que comemos y los alimentos que elegimos . Aprende a manejarlas y reconcíliate con la comida.'
                         />
                         <Servicio
+                            icon={plan}
                             tittle={'Plan de alimentación personalizado'}
-                            description='Lorem'
+                            description='La cantidad y calidad de alimentos es diferente para cada uno de nosotros. Nuestros planes de alimentación son personalizados de acuerdo a los gustos, preferencias , cultura , edad, actividades, ejercicio etc . Todos los organismos son diferentes.'
                         />
                         <Servicio
-                            tittle={'Venta de productos saludables y suplementos'}
-                            description='Lorem'
+                            icon={venta}
+                            tittle={'Venta de productos saludables'}
+                            description='Tienda en nuestra clínica nutricional.'
                         />
                     </div>
                 </section>
