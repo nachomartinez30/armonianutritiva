6aee9749d4ff3cc6bfb36366b07091cf87e9161a
nachomartinez30 <nachomartinez3010@gmail.com>
2020-12-09 09:15:50 -0600

    hotfix colors

--- src/Descripcion.jsx
+++ src/Descripcion.jsx
@@ -1,11 +1,11 @@
 const Descripcion = () => {
     return (
         <section className='py-10 mx-auto px-4 justify-items-auto'>
-            <h1 className='text-cafe text-5xl text-center '>
+            <h1 className='text-cafe text-5xl text-center text-cafe-dark'>
                 <span className='text-rosa-DEFAULT font-bold'>Armonía </span>
                 <span className='text-rosa-DEFAULT'> Nutritiva </span> es:
             </h1>
-            <p className='pt-10 px-8 text-2xl text-justify'>
+            <p className='pt-10 px-8 text-2xl text-justify text-cafe-dark'>
                 Un consultorio de <span className='text-rosa-DEFAULT script-font text-4xl font-bold'> nutrición </span> que se enfoca en obtener la mejor versión del paciente, tratando aspectos <span className='font-bold script-font text-4xl text-verde-DEFAULT'> emocionales </span> y <span className='script-font text-4xl font-bold text-amarillo-DEFAUL'> físicos </span> para mejorar su calidad , estilo de vida y la relación que tenga con el mismo, la imagen corporal y la comida.
             </p>
         </section>
