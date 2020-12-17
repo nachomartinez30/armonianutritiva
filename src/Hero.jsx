d19ec19e3b8d423547e8fb4436f98e0dea4b0018
nachomartinez30 <nachomartinez3010@gmail.com>
2020-12-17 17:15:43 -0600

    imagenes optimizadas

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
             <div className="flex">
                 <div className="rounded-lg h-screen w-full">
                     <div className="pt-48 px-5 flex flex-grow flex-col align-middle bg-fixed mBackground-image h-full w-full">
-                        <img className='fade-in pl-4' src={logo} alt='Logo' />
+                        <img className='fade-in pl-4' loading='lazy' src={logo} alt='Logo' />
                     </div>
                 </div>
             </div>
